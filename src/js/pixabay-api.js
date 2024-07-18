97c6d9c90fc9a3e5f50bad02ee7c51197964a609
Oleksandr Kurchenko <151569683+AlexSaymon@users.noreply.github.com>
2024-07-17 20:33:14 -0500

    homework update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,28 +1,12 @@
 const API_KEY = `44952732-afe4551e0894d66c0b6ee4476`;
-const URL = `https://pixabay.com/api/`;
 
 export function fetchImages(query) {
-  const url = `${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
-  const options = {
-    method: `GET`,
-    headers: {
-      'Content-Type': `application/json`,
-    },
-  };
+  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
 
-  console.log('Fetching URL:', url);
-  console.log('Options:', options);
-
-  return fetch(url, options)
-    .then(res => {
-      if (!res.ok) {
-        throw new Error(res.status);
-      }
-      return res.json();
-    })
-    .then(data => data.length)
-
-    .catch(error => {
-      console.error(error);
-    });
+  return fetch(url).then(res => {
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res.json();
+  });
 }
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ const elements = {
   submitBtn: document.querySelector(`.js-submit-btn`),
   form: document.querySelector(`form[data-form]`),
   input: document.querySelector(`input[data-input]`),
+  gallery: document.querySelector(`.gallery`),
 };
 
 elements.submitBtn.addEventListener(`click`, event => {
@@ -12,18 +13,59 @@ elements.submitBtn.addEventListener(`click`, event => {
 
   const trimmedInput = elements.input.value.trim();
   const userInputValue = elements.input.value;
-  if (trimmedInput === '') {
-    elements.submitBtn.disabled = true;
-    console.log(`button is disabled`);
-    return;
-  } else {
-    elements.submitBtn.disabled = false;
-  }
-  fetchImages(userInputValue).then(res => {
-    if (!res || res.length === 0) {
+
+  function emptyInputInfo() {
+    if (trimmedInput === '') {
+      elements.submitBtn.disabled = true;
+      console.log(`input is empty`);
       noImagesMessage();
-    } else {
-      console.log(res);
+      return true;
     }
-  });
+    elements.submitBtn.disabled = false;
+    return false;
+  }
+
+  function createImageCard({
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads,
+  }) {
+    return `<div class="image-card">
+    <img src="${largeImageURL}" alt= "${tags}" />
+    <div class="info">
+    <p>Likes: ${likes}</p>
+    <p>Views: ${views}</p>
+    <p>Comments: ${comments}</p>
+    <p>Downloads: ${downloads}</p>
+    </div>
+  </div>`;
+  }
+
+  function processImages(res) {
+    const images = res.hits;
+    let imageMarkup = '';
+    images.forEach(image => {
+      imageMarkup += createImageCard(image);
+    });
+    elements.gallery.innerHTML = imageMarkup;
+  }
+
+  elements.gallery.innerHTML = '';
+  elements.form.reset();
+
+  if (emptyInputInfo()) {
+    elements.submitBtn.disabled = false;
+    return;
+  }
+
+  fetchImages(userInputValue)
+    .then(processImages)
+
+    .catch(error => {
+      console.error(`No Images`, error);
+    });
 });
